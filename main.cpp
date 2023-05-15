d70b51aa77a23524e25a3eddefda4c1e737338cb
BigSnowHill <semiklit.katya@gmail.com>
2023-05-15 14:44:32 +0300

    Output, index operator

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@ private:
     size_t N, M;
     std::vector<std::vector<T>> matrix;
 public:
-    explicit Matrix(size_t n, size_t m, std::vector<std::vector<T>> new_matrix): N(n), M(m), matrix(new_matrix){};
+    explicit Matrix(size_t n, size_t m, std::vector<std::vector<T>> new_matrix) : N(n), M(m), matrix(new_matrix) {};
 
-    Matrix(size_t n, size_t m) : N(n), M(m){
+    Matrix(size_t n, size_t m) : N(n), M(m) {
         for (size_t i = 0; i < N; ++i) {
             std::vector<T> elem_vector;
-            for(size_t j = 0; j < M; ++j){
+            for (size_t j = 0; j < M; ++j) {
                 T element;
                 std::cin >> element;
                 elem_vector.push_back(element);
@@ -23,11 +23,11 @@ public:
         }
     }
 
-    Matrix(){
+    Matrix() {
         std::cin >> N >> M;
         for (size_t i = 0; i < N; ++i) {
             std::vector<T> elem_vector;
-            for(size_t j = 0; j < M; ++j){
+            for (size_t j = 0; j < M; ++j) {
                 T element;
                 std::cin >> element;
                 elem_vector.push_back(element);
@@ -36,12 +36,12 @@ public:
         }
     }
 
-    Matrix(const std::string& filename){
+    Matrix(const std::string &filename) {
         std::ifstream file_in(filename);
         file_in >> N >> M;
         for (size_t i = 0; i < N; ++i) {
             std::vector<T> elem_vector;
-            for(size_t j = 0; j < M; ++j){
+            for (size_t j = 0; j < M; ++j) {
                 T element;
                 file_in >> element;
                 elem_vector.push_back(element);
@@ -50,10 +50,37 @@ public:
         }
     }
 
+    size_t get_lines_number() {
+        return N;
+    }
+
+    size_t get_colomns_number() {
+        return M;
+    }
+
+
+    std::vector<T> &operator[](size_t i) {
+        return matrix[i];
+    }
+
+
 };
+
+
+template<class T>
+std::ostream &operator<<(std::ostream &out, Matrix<T> A) {
+    for (size_t i = 0; i < A.get_lines_number(); ++i) {
+        for (size_t j = 0; j < A.get_colomns_number(); ++j) {
+            out << A[i][j] << " ";
+        }
+        out << "\n";
+    }
+    return out;
+}
 
 
 int main() {
     Matrix<int> a;
+    std::cout << a;
     return 0;
 }
